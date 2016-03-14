8fe4673b4830b1150e83541f18c8bf26bc2b4541
alanclarke <hi@alz.so>
2016-03-14 16:28:20 +0000

    standard

---
+++ test/test-domains.js
@@ -0,0 +1,24 @@
+/* global describe it expect */
+var getDomains = require('../lib/domains')
+
+describe('getDomains', function () {
+  it('should return permutations of subdomains', function () {
+    expect(getDomains('just.keep.coding.com')).to.contain('just.keep.coding.com')
+    expect(getDomains('just.keep.coding.com')).to.contain('keep.coding.com')
+    expect(getDomains('just.keep.coding.com')).to.contain('coding.com')
+  })
+
+  it('should return permutations of subdomains inc dot', function () {
+    expect(getDomains('just.keep.coding.com')).to.contain('.just.keep.coding.com')
+    expect(getDomains('just.keep.coding.com')).to.contain('.keep.coding.com')
+    expect(getDomains('just.keep.coding.com')).to.contain('.coding.com')
+  })
+
+  it('should return a null value', function () {
+    expect(getDomains('just.keep.coding.com')).to.contain(null)
+  })
+
+  it('should return nothing else', function () {
+    expect(getDomains('just.keep.coding.com')).to.have.length(7)
+  })
+})
---
+++ test/test-paths.js
@@ -0,0 +1,13 @@
+/* global describe it expect */
+var getPaths = require('../lib/paths')
+
+describe('getPaths', function () {
+  it('should return / in absence of path', function () {
+    expect(getPaths('/')).to.eql(['/'])
+    expect(getPaths('')).to.eql(['/'])
+  })
+
+  it('should return a permutation of path parts', function () {
+    expect(getPaths('/to/enlightenment')).to.eql(['/', '/to', '/to/enlightenment'])
+  })
+})
