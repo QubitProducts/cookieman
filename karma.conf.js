04f2f98fe8664bf948ad7bd98d70fab8431f3912
alanclarke <alan@qubit.com>
2021-05-13 10:52:56 +0100

    Audit, CI, healthier, prettier-standard

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,15 @@ module.exports = function (config) {
   config.set({
     urlRoot: '/cookie/man',
     frameworks: ['mocha', 'expect', 'sinon'],
-    files: [ 'test/**/test-*.js' ],
+    files: ['test/**/test-*.js'],
     preprocessors: { 'test/**/test-*.js': ['webpack', 'sourcemap'] },
     webpack: {
       watch: true,
       devtool: 'inline-source-map'
     },
-    webpackServer: {
-      quiet: true,
-      noInfo: true
+    webpackMiddleware: {
+      stats: 'errors-only',
+      logLevel: 'error'
     },
     reporters: ['spec'],
     browsers: ['Chrome']
